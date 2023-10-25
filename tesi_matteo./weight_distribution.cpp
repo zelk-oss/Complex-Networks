34f24e79fd1a557cd226ce81b5347729636d8481
zelk-oss <zelcochiara@gmail.com>
2023-10-25 15:00:08 +0200

    changes

--- tesi_matteo./weight_distribution.cpp
+++ tesi_matteo./weight_distribution.cpp
@@ -16,15 +16,17 @@ int main()
     assert(Corruzione >= 0 && Corruzione <= 1);
 
     double Temp = 0.2; // temperature
-    int N_pattern = 0;
+    int N_pattern = 20;
     std::fstream file_FA;
 
     Hopfield_Network hopfield(N_quad, Temp); // network creation
 
     // I add a memories to the network and check the weight distribution. I reiterat it many times
 
-    for (int k = 1; k < 0.2 * N_quad; k++)
-    {
+    //for (int k = 1; k < 0.2 * N_quad; k++)
+    for (int j = 1; 100; j++) {
+    for (int k = 1; N_pattern; k++) 
+    {    
         std::vector<int> pattern(N_quad, +1);
         for (int j = 0; j < pattern.size(); j++) // creation of a random pattern
         {
@@ -39,9 +41,13 @@ int main()
             }
         }
         hopfield.AddPattern(pattern);
+    } // generated N_pattern memories 
+
+    // i would like to put generated weights into columns to produce the histo 
+    } // repeated generation 100 times 
 
         char filename[100];
-        sprintf(filename, "Weight_Distribution/N_625/Pattern_%d.txt", k);
+        sprintf(filename, "Weight_Distribution/N_625/Pattern_%d.txt");
         file_FA.open(filename, std::fstream::out);
         file_FA << "peso" << '\t' << "valore" << '\n';
         for (int i = 0; i < hopfield.Get_Weights_Size(); i++)
@@ -49,5 +55,4 @@ int main()
             file_FA << i << ';' << hopfield.Get_Weight(i) << '\n';  
         }
         file_FA.close();
-    }
 }

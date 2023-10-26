5dd3cac2e1b2388bbd97985c72be3b582da31f20
Matteo Shqemza <matteoshqemza@gmail.com>
2023-10-26 12:53:57 +0200

    working weight_distribution.cpp

--- tesi_matteo./weight_distribution.cpp
+++ tesi_matteo./weight_distribution.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 #include <cassert>
 #include <algorithm>
 #include "Hopfield_model.hpp"
@@ -10,51 +11,49 @@ int main()
 
     int N_quad = 625; // number of neurons
 
-    double Corruzione = 0;
-    std::cout << "percentuale di corruzione:" << '\n';
-    std::cin >> Corruzione;
-    assert(Corruzione >= 0 && Corruzione <= 1);
-
     double Temp = 0.2; // temperature
     int N_pattern = 20;
     std::fstream file_FA;
 
-    Hopfield_Network hopfield(N_quad, Temp); // network creation
+    std::filesystem::create_directory("Weight_Distribution_N_625");
 
-    // I add a memories to the network and check the weight distribution. I reiterat it many times
-
-    //for (int k = 1; k < 0.2 * N_quad; k++)
-    for (int j = 1; 10; j++) {
-    for (int k = 1; N_pattern; k++) 
-    {    
-        std::vector<int> pattern(N_quad, +1);
-        for (int j = 0; j < pattern.size(); j++) // creation of a random pattern
+    // for (int k = 1; k < 0.2 * N_quad; k++)
+    for (int a = 1; a < 10; a++)
+    {
+        Hopfield_Network hopfield(N_quad, Temp); // network creation
+        // I add a memories to the network and check the weight distribution. I reiterat it many times
+        for (int k = 1; k < N_pattern; k++) // the network will learn N_pattern memories
         {
-            double caso = (double)rand() / RAND_MAX;
-            if (caso < 0.5) // p(+1) = 1/2
+            std::vector<int> pattern(N_quad, +1);
+            for (int j = 0; j < pattern.size(); j++) // creation of a random pattern
             {
-                pattern[j] = -1;
+                double caso = (double)rand() / RAND_MAX;
+                if (caso < 0.5) // p(+1) = 1/2
+                {
+                    pattern[j] = -1;
+                }
+                else
+                {
+                    pattern[j] = +1;
+                }
             }
-            else
-            {
-                pattern[j] = +1;
-            }
-        }
-        hopfield.AddPattern(pattern);
-    } // generated N_pattern memories 
+            hopfield.AddPattern(pattern);
+        } // generated N_pattern memories
 
-    // i would like to put generated weights into columns to produce the histo 
-    } // repeated generation 100 times 
+        // i would like to put generated weights into columns to produce the histo
+        // I save all the 100 outcome for just for five differnte weights
 
         char filename[100];
-        sprintf(filename, "Weight_Distribution/N_625_Pattern_%d.txt", N_pattern);
+        sprintf(filename, "Weight_Distribution_N_625/Pattern_%d_%d.txt", N_pattern, a);
         file_FA.open(filename, std::fstream::out);
-        file_FA << "peso" << '\t' << "valore" << '\n'; // questa è la prima riga del file. 
-        //for (int i = 0; i < hopfield.Get_Weights_Size(); i++)
-        for (int i = 0; i < 10; i++)
+        file_FA << "peso 1" << '\t' << "peso 2 " << '\t' << "peso 3" << '\t' << "peso 4" << '\t' << "peso 5" << '\n'; // first raw of the file.
+        file_FA.close();
 
+        file_FA.open(filename, std::fstream::app);
+        for (int i = 0; i < hopfield.Get_Weights_Size(); i++)
         {
-            file_FA << i << ';' << hopfield.Get_Weight(i) << '\n';  
+            file_FA << i << ';' << hopfield.Get_Weight(i) << '\n';
         }
-        file_FA.close(); // il file quindi avrà due colonne, una con indice del peso a cui facciamo riferimento, l'altra con il peso nella posizione di riferimento 
+        file_FA.close(); // il file quindi avrà cinque colonne, una per ogni peso
+    }                    // repeated generation 100 times
 }

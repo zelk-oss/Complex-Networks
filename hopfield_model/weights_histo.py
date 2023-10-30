843be10c8a33b51f6facfa766541641223304d68
zelk-oss <zelcochiara@gmail.com>
2023-10-30 13:15:41 +0100

    gaussian fit

--- hopfield_model/weights_histo.py
+++ hopfield_model/weights_histo.py
@@ -2,44 +2,52 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-# Funzione gaussiana per il fit
 def gaussian(x, A, mu, sigma):
     return A * np.exp(-(x - mu)**2 / (2 * sigma**2))
 
-# Inizializza le liste per gli istogrammi
-histograms = []
-current_histogram = []
+bin_values = []
+entries = []
 
-# Leggi il file CSV con tabulazioni come delimitatori
-with open('dati.csv', 'r') as file:
+histograms = []
+
+figure_counter = 1
+
+with open('dati.txt', 'r') as file:
     for line in file:
         if line.strip():  # Ignora le linee vuote
-            bin_value, entry = map(int, line.strip().split('\t'))
+            bin_value, entry = map(int, line.strip().split(','))
 
             if bin_value == 0:
                 # Inizia un nuovo istogramma
-                if current_histogram:
-                    histograms.append(current_histogram)
-                current_histogram = []
+                if bin_values:
+                    histograms.append((bin_values, entries))
+                    bin_values = []
+                    entries = []
 
-            current_histogram.extend([bin_value] * entry)
+            bin_values.append(bin_value)
+            entries.append(entry)
 
-# Aggiungi l'ultimo istogramma
-histograms.append(current_histogram)
+histograms.append((bin_values, entries))
 
-# Fai il fit gaussiano e disegna gli istogrammi
-for i, histogram in enumerate(histograms):
-    x = np.arange(len(histogram))
-    y = np.array(histogram)
-    initial_guess = [max(y), np.argmax(y), np.std(y)]
+# Fai il fit gaussiano e disegna gli istogrammi in figure separate
+for i, (bin_values, entries) in enumerate(histograms):
+    bin_values = np.array(bin_values)
+    entries = np.array(entries)
 
-    popt, _ = curve_fit(gaussian, x, y, p0=initial_guess)
+    popt, _ = curve_fit(gaussian, bin_values, entries, p0=[max(entries), np.mean(bin_values), np.std(bin_values)])
 
-    plt.figure()
-    plt.bar(x, y, alpha=0.5, label='Istogramma')
-    plt.plot(x, gaussian(x, *popt), 'r', label='Fit gaussiano')
-    plt.title(f'Istogramma {i + 1}')
+    plt.figure(figure_counter)
+    plt.bar(bin_values, entries, alpha=0.5, label=f'Istogramma {i + 1}')
+
+    x_fit = np.linspace(min(bin_values), max(bin_values), 100)
+    y_fit = gaussian(x_fit, *popt)
+    plt.plot(x_fit, y_fit, label=f'Fit Gaussiano {i + 1}')
+
+    plt.title(f'Istogramma {i + 1} con Fit Gaussiano')
     plt.xlabel('Bin')
     plt.ylabel('Entries')
     plt.legend()
+
     plt.show()
+
+    figure_counter += 1

ca79cad135a21a074a91f860fed1b64539b26a92
zelk-oss <zelcochiara@gmail.com>
2023-10-31 18:52:08 +0100

    commit

--- hopfield_model/weight_distribution.cpp
+++ hopfield_model/weight_distribution.cpp
@@ -1,130 +1,174 @@
-#include <iostream>
-#include <fstream>
-#include <string>
+#include "tesi_matteo/Hopfield_model.hpp"
 #include <cmath>
-#include "Hopfield_model.hpp"
+#include <fstream>
+#include <iostream>
+#include <string>
 
-int main()
-{
+int main() {
 
-    /********************
-     * inizialization of the parameter
-     *******************/
+  /********************
+   * inizialization of the parameter
+   *******************/
 
-    srand(time(NULL));
+  srand(time(NULL));
 
-    int const N_quad = 400; // number of neurons
-    int const N_link = N_quad * N_quad;
-    double Temp = 0.2;        // temperature
-    int const N_pattern = 20; // number of patter
-    double link_probability;
-    double link_entropy;
-    std::vector<std::vector<int>> histograms(N_link); // vector of histograms for the different weight
-    std::vector<std::vector<int>> entropies(N_link);  // vector of histograms for the different weight
-    std::fstream file_histo;
-    std::fstream file_entropy;
+  int const N_quad = 100; // number of neurons
+  int const N_link = N_quad * N_quad;
+  int const N_generations = 100;
+  double Temp = 0.2;        // temperature
+  //int const N_pattern = 20; // number of patter
+  double link_probability;
+  double link_entropy;
+  std::vector<std::vector<int>> histograms(
+      N_link); // vector of vectors (histograms) for the different weights. dim
+               // = N_link
+  // will have to removes symmetric elements and the diagonal later
+  std::vector<std::vector<int>> entropies(
+      N_link); // vector of entropy values for the different weights
+  std::fstream file_histo;
+  std::fstream file_gauss;
+  std::fstream file_entropy;
+  double entropy_Np; // entropy as function of patterns number 
 
-    /********************
-     * creating histograms
-     ********************/
+  /********************
+   * creating histograms
+   ********************/
+// iterate over variable N_pattern numbers 
+for (int N_pattern = 20; N_pattern < 30; N_pattern++) {
 
-    for (int i = 0; i < 2 * N_pattern + 1; i++)
-    { // inizialization of the single histogram with fixed bin
-        for (int j = 0; j < histograms.size(); j++)
-        {
-            histograms[j].push_back(0);
-        }
+  for (int i = 0; i < 2 * N_pattern + 1;
+       i++) // max weight = p patterns, minimum = -p. 2N_p + 1 = length
+  { // inizialization of the single histogram with fixed bin: set number of bins
+    for (int j = 0; j < histograms.size(); j++) {
+      histograms[j].push_back(0);
     }
-    std::string filename_histo = "histogram.csv"; // create the output file
-    file_histo.open(filename_histo, std::fstream::out);
-    file_histo << "bin" << '\t' << "occurrences" << '\n'; // first raw of the file.
-    file_histo.close();
+  }
+  // produces N_link lines of 41 0s. so we are ready to fill them up.
 
-    /********************
-     *creation entropy repository
-     ********************/
+  std::string filename_histo = "histogram.csv"; // create the output file
+  file_histo.open(filename_histo, std::fstream::out);
+  file_histo << "bin"
+             << ","
+             << "occurrences" << '\n'; // first row of the file.
+  file_histo.close();
 
-    for (int i = 0; i < 2 * N_pattern + 70; i++)
-    { // inizialization of the single histogram with fixed bin
-        for (int j = 0; j < entropies.size(); j++)
-        {
-            entropies[j].push_back(0);
-        }
+  std::string filename_gauss = "hist_for_gauss.csv";
+  file_gauss.open(filename_gauss, std::fstream::out);
+  file_gauss << "bin, occurrences" << '\n';
+  file_gauss.close();
+
+  /********************
+   *creation of entropy repository
+   ********************/
+
+  for (int i = 0; i < 2 * N_pattern + 1;
+       i++) { // inizialization of the single histogram with fixed bin
+    for (int j = 0; j < entropies.size(); j++) {
+      entropies[j].push_back(0);
     }
-    std::string filename_entropy = "entropy.csv";
-    file_histo.open(filename_entropy, std::fstream::out);
-    file_histo << "bin" << '\t' << "entropy" << '\n'; // first raw of the file.
-    file_histo.close();
+  }
+  std::string filename_entropy = "entropy.csv";
+  file_histo.open(filename_entropy, std::fstream::out);
+  file_histo << "N_patterns"
+             << ","
+             << "Entropy" << '\n';
+  file_histo.close();
+  // same stuff as histo.
 
-    /********************
-     * I create a hopfield network, teach it N_pattern random genareted memories and count the occurence of the weight
-     * I repeat it 100 times
-     *******************/
+  /********************
+   * I create a hopfield network, teach it N_pattern random genareted memories
+   *and count the occurence of the weights I repeat this procedure N_generations
+   *times
+   *******************/
+  for (int a = 1; a <= N_generations; a++) // set number of generations
+  {
+    Hopfield_Network hopfield(N_quad, Temp); // network creation
 
-    for (int a = 1; a <= 1000; a++) // set number of generations
+    // I add memories to the network and check the weight distribution. I
+    // reiterate this many times
+
+    for (int k = 0; k < N_pattern;
+         k++) // the network will learn N_pattern memories
     {
-        Hopfield_Network hopfield(N_quad, Temp); // network creation
-
-        // I add a memories to the network and check the weight distribution. I reiterat it many times
-
-        for (int k = 0; k < N_pattern; k++) // the network will learn N_pattern memories
+      std::vector<int> pattern(N_quad, +1); // N_quad elements initialized at 1
+      for (int j = 0; j < pattern.size(); j++) // creation of a random pattern
+      {
+        double caso = (double)rand() / RAND_MAX;
+        if (caso < 0.5) // p(+1) = 1/2
         {
-            std::vector<int> pattern(N_quad, +1);
-            for (int j = 0; j < pattern.size(); j++) // creation of a random pattern
-            {
-                double caso = (double)rand() / RAND_MAX;
-                if (caso < 0.5) // p(+1) = 1/2
-                {
-                    pattern[j] = -1;
-                }
-                else
-                {
-                    pattern[j] = +1;
-                }
-            }
-            hopfield.AddPattern(pattern);
-        } // generated N_pattern memories
-        // I count all the occurencies for a singles weight
-        for (int i = 0; i < histograms.size(); i++)
-        {
-            int bin = static_cast<int>(hopfield.Get_Weight(i) * N_quad) + N_pattern;
-            ++histograms[i][bin];
+          pattern[j] = -1;
+        } else {
+          pattern[j] = +1;
         }
-        std::cout << "finish " << a << " iteration\n";
-
-    } // repeated generation 1000 times
-
-    /********************
-     *  I export the data
-     *******************/
-
-    file_histo.open(filename_histo, std::fstream::app);
-    for (int i = 0; i < histograms.size(); i++) // cicle over all the weight
-    {
-        for (int j = 0; j < histograms[i].size(); j++) // cicle over all the possible outcome
-        {
-            file_histo << j << '\t' << histograms[i][j] << std::endl; // print the value
-        }
-        file_histo << '\n';
+      }
+      hopfield.AddPattern(pattern);
+    } // generated N_pattern memories
+    // I count all the occurencies for a singles weight
+    for (int i = 0; i < histograms.size(); i++) {
+      int bin = static_cast<int>(hopfield.Get_Weight(i) * N_quad) + N_pattern;
+      ++histograms[i][bin];
     }
-    file_histo.close();
 
-    /********************
-     * find and export the entropy
-     *******************/
+  } // repeated generation 1000 times
 
-    file_entropy.open(filename_entropy, std::fstream::app);
-    for (int i = 0; i < N_link; i++) // cicle overe all the raw
+  /********************
+   *  I export the data
+   *******************/
+  file_histo.open(filename_histo, std::fstream::out);
+  file_gauss.open(filename_gauss, std::fstream::out);
+  for (int i = 0; i < N_quad; i++) // cicle over all the rows
+  {
+    for (int j = i + 1; j < N_quad; j++) // cicle over the columns
     {
-        for (int j = i + 1; j < N_link;) // cicle over the upper triangular matrix (we cancel the trace and the symmetry)
-        {
-            for (int k = 0; k < histograms[i].size(); k++) // cicle over the possible outcome
-            {
-                link_probability = histograms[i * N_link + j][k] / 100.;      // find the probability distribution
-                link_entropy = link_probability * std::log(link_probability); // find the entropy per outcome per link
-                file_entropy << j << '\t' << link_entropy << '\n';            // print the value
-            }
-        }
+      for (int j = 0; j < histograms[i].size();
+           j++) // cicle over all the possible outcomes for one weight
+      {         // note: size of histograms[i] is 41.
+                // print upper triangular histograms
+        file_histo << j << "," << histograms[i][j] << '\n';
+        if ((j % 2) == 0) {
+          file_gauss << j/2 << "," << histograms[i][j] << '\n';
+        } else {
+        } // remove empty bins
+      }
     }
-    file_entropy.close();
+  }
+  file_histo.close();
+  file_gauss.close();
+
+  /********************
+   * find and export the entropy
+   *******************/
+
+  file_entropy.open(filename_entropy, std::fstream::out);
+  for (int i = 0; i < N_quad; i++) // cicle overe all the rows
+  {
+    for (int j = i + 1; j < N_quad;
+         j++) // cicle over the upper triangular matrix (we cancel the trace and
+              // the symmetry)
+    {
+      for (int k = 0; k < histograms[i].size();
+           k++) // cicle over the possible weight outcomes
+      {
+        if (histograms[i * N_quad + j][k] == 0) {
+        } // skip probability == 0 cases. we should get rid of nan-s.
+        else {
+          link_probability = histograms[i * N_quad + j][k] /
+                             static_cast<double>(N_generations);
+          link_entropy =
+              (-1) * link_probability *
+              std::log(
+                  link_probability); // find the entropy per outcome per link
+          
+          entropy_Np += link_entropy;
+        }
+      } // before ending the total loop i need to push stuff inside the file 
+    }
+  }
+  file_entropy << N_pattern << "," << entropy_Np << '\n';
+  file_entropy.close();
+}
+// re-set variables to zero for next cycle. 
+link_entropy = 0; 
+link_probability = 0; 
+entropy_Np = 0; 
 }
--- hopfield_model/weights_histo.py
+++ hopfield_model/weights_histo.py
@@ -12,7 +12,7 @@ histograms = []
 
 figure_counter = 1
 
-with open('dati.txt', 'r') as file:
+with open('hist_for_gauss.csv', 'r') as file:
     for line in file:
         if line.strip():  # Ignora le linee vuote
             bin_value, entry = map(int, line.strip().split(','))
@@ -30,24 +30,27 @@ with open('dati.txt', 'r') as file:
 histograms.append((bin_values, entries))
 
 # Fai il fit gaussiano e disegna gli istogrammi in figure separate
+hist_len = len(histograms)
+trial_plots = [1, 1000, 10000, 100000, 1000000]
 for i, (bin_values, entries) in enumerate(histograms):
-    bin_values = np.array(bin_values)
-    entries = np.array(entries)
+    
+    if i in trial_plots: 
+        bin_values = np.array(bin_values)
+        entries = np.array(entries)
 
-    popt, _ = curve_fit(gaussian, bin_values, entries, p0=[max(entries), np.mean(bin_values), np.std(bin_values)])
+        #popt, _ = curve_fit(gaussian, bin_values, entries, p0=[max(entries), np.mean(bin_values), np.std(bin_values)])
 
-    plt.figure(figure_counter)
-    plt.bar(bin_values, entries, alpha=0.5, label=f'Istogramma {i + 1}')
+        plt.figure(figure_counter)
+        plt.bar(bin_values, entries, alpha=0.5, label=f'Istogramma {i + 1}')
 
-    x_fit = np.linspace(min(bin_values), max(bin_values), 100)
-    y_fit = gaussian(x_fit, *popt)
-    plt.plot(x_fit, y_fit, label=f'Fit Gaussiano {i + 1}')
+        #x_fit = np.linspace(min(bin_values), max(bin_values), 100)
+        #y_fit = gaussian(x_fit, *popt)
+        #plt.plot(x_fit, y_fit, label=f'Fit Gaussiano {i + 1}')
 
-    plt.title(f'Istogramma {i + 1} con Fit Gaussiano')
-    plt.xlabel('Bin')
-    plt.ylabel('Entries')
-    plt.legend()
-
-    plt.show()
-
+        plt.title(f'Istogramma {i + 1} con Fit Gaussiano')
+        plt.xlabel('Bin')
+        plt.ylabel('Entries')
+        plt.legend()
+    
+        plt.show()
     figure_counter += 1

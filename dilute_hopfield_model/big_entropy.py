b7c6886ce8776313303938c8b7df01c5050c8a3a
Matteo Shqemza <matteoshqemza@gmail.com>
2023-11-07 15:52:40 +0100

    funziona

--- dilute_hopfield_model/big_entropy.py
+++ dilute_hopfield_model/big_entropy.py
@@ -5,22 +5,28 @@ import scipy
 
 # this script will open the file with entropy points as function of number of patterns 
 # and plot the points. 
-x_values = []
-y_values = []
-z_values = []
+N_pattern = []
+prob_creation = []
+entropies = []
+x = []
+y = []
 
 with open('all_parameter_entropy.csv', 'r') as file:
     next(file)
     for line in file:
-        if line.strip():
-            x, y, z = line.strip().split(",")
-            x_values.append(float(x))
-            y_values.append(1. - float(y)) 
-            z_values.append(float(z))
+        row = line.strip().split(",")
+        N_pattern.append(float(row[0]))
+        prob_creation.append(1. - float(row[1]))
+        entropies.append(float(row[2]))
 
-plt.plot(x_values, z_values, marker='o', linestyle='-')
+for element in range(len(prob_creation)):
+    if round(prob_creation[element],2) == 0.01:
+        x.append(N_pattern[element])
+        y.append(entropies[element])
+
+plt.plot(x, y, marker='o', linestyle='-')
 plt.title('Entropy(p)')
-plt.xlabel('probabilità di distuzione')
-plt.ylabel('Entropy')
+plt.xlabel('N_pattern')
+plt.ylabel('Entropy')   
 plt.grid(True)
 plt.show()
